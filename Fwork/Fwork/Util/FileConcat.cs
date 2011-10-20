15085482a2f243a0a6b47c4a890bb7521a421e06
bruno-campos <brunocampos@gmail.com>
2011-10-20 12:13:24 -0200

    FileReader e FileConcat

---
+++ Fwork/Fwork/Util/FileConcat.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fwork.Util {
+	
+	public class FileConcat {
+		
+		private List<string> files;
+		
+		public FileConcat() {
+			this.files = new List<string>();
+		}
+		
+		public void AddFile(string filename) {
+			this.files.Add(filename);
+		}
+		
+		public void RemoveFile(string filename) {
+			this.files.Remove(filename);
+		}
+		
+		public void CreateFile(string filename) {
+			string filecode = "";
+			foreach(string file in this.files) {
+				filecode += this.getCode(file) + "\r\n";
+			}
+			FileWriter FW = new FileWriter(filename);
+			try {
+				FW.Write(filecode);
+			} finally {
+				FW = null;
+			}
+		}
+		
+		private string getCode(string filename) {
+			string code;
+			FileReader FR = new FileReader(filename);
+			try {
+				code = FR.Read();
+				return code;
+			} finally {
+				FR = null;
+			}
+		}
+		
+	}
+	
+}
---
+++ Fwork/Fwork/Util/FileReader.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Fwork.Util {
+	
+	public class FileReader {
+		
+		private string filepath;
+		
+		public FileReader(string filepath) {
+			this.filepath = filepath;
+		}
+		
+		public string Read() {
+			StreamReader sr = new StreamReader(this.filepath);
+			try {
+				string str = sr.ReadToEnd();
+				return str;
+			} finally {
+				sr.Close();
+				sr.Dispose();
+			}
+		}
+		
+	}
+	
+}
